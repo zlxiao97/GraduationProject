da657bade182ef91dd9ab9f02847c17cd398e0bb
2552895864 <2552895864@qq.com>
2020-02-09 14:08:52 +0800

    调整api返回类型

--- APITest/router/faceset.js
+++ APITest/router/faceset.js
@@ -10,28 +10,28 @@ const search = require("../api/searchByFace.js");
 module.exports = (key)=>{
     router.post('/add',function(req,res){
         add(req,key).then(data=>{
-            res.send(JSON.parse(data));
+            res.json(JSON.parse(data));
         }).catch(err=>{
             res.send(JSON.stringify(err));
         });
     });
     router.post('/get',function(req,res){
         get(req,key).then(data=>{
-            res.send(JSON.parse(data));
+            res.json(JSON.parse(data));
         }).catch(err=>{
             res.send(JSON.stringify(err));
         });
     });
     router.post('/delete',function(req,res){
         del(req,key).then(data=>{
-            res.send(JSON.parse(data));
+            res.json(JSON.parse(data));
         }).catch(err=>{
             res.send(JSON.stringify(err));
         });
     });
     router.post('/search',function(req,res){
         search(req,key).then(data=>{
-            res.send(JSON.parse(data));
+            res.json(JSON.parse(data));
         }).catch(err=>{
             res.send(JSON.stringify(err));
         });
