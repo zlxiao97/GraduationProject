8fcd626a4ed4eb30cda5fe682b56ecbfe8160876
肖子立 <xiaozl1@digitalchina.com>
2020-03-25 23:55:07 +0800

    跨域

--- APITest/app.js
+++ APITest/app.js
@@ -21,10 +21,10 @@ app.all("*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+    "Content-Type, Content-Length, Authorization, Accept, X-Requested-With, Cache-Control, Pragma, Referer, User-Agent"
   );
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("X-Powered-By", " 3.2.1");
   if (req.method == "OPTIONS") res.send(200);
   else next();
