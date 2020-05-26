01529172e209e7d350f31708f665e5133a8fefbf
2552895864 <zlxiao97@yahoo.com>
2020-05-26 21:31:25 +0800

    修复移动端修改密码无权限bug

--- APITest/token/index.js
+++ APITest/token/index.js
@@ -2,7 +2,7 @@ const expressJwt = require("express-jwt");
 const { secretKey } = require("./constant.js");
 
 const jwtAuth = expressJwt({ secret: secretKey }).unless({
-  path: ["/manage/login", "/student/login", "/upload"]
+  path: ["/manage/login", "/student/login", "/upload", "/student/updatePwd"],
 });
 
 module.exports = jwtAuth;
